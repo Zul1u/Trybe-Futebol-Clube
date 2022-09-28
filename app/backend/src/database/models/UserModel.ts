9cfeff2588dde0128fbb237b68af2736893e4400
Arthur <martinsarthur07@gmail.com>
2022-09-28 18:15:13 -0300

    feat: cria a migraton e o model da tabela users

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,41 @@
+import { INTEGER, Model, STRING } from 'sequelize/types';
+import db from '.';
+
+class Users extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  modelName: 'users',
+});
+
+export default Users;
