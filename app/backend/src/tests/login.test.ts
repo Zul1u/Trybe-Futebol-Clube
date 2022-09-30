b94b5e04b9a46ea1da93a706ee93af311812bff0
Arthur <martinsarthur07@gmail.com>
2022-09-30 19:17:15 -0300

    feat: adiciona os testes de quando o login falha

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -27,22 +27,82 @@ const userLogin = {
   password: 'secret_user'
 }
 
+const incorrectLogins = [
+  { password: 'secret_user' },
+  { email: 'user@user.com', },
+  { email: 'iuser@iuser.com', password: 'secret_user' },
+  { email: 'user@user.com', password: 'secret_use' }
+];
 
+const fieldFillMessage = 'All fields must be filled';
+const incorrectDataMessage = 'Incorrect email or password';
 
 describe('/login', () => {
   describe('POST', () => {
-    before(() => {
-      sinon.stub(User, 'findOne').resolves(userIfos as User);
+    describe('Quando a função findOne faz uma busca com sucesso', () => {
+      before(() => {
+        sinon.stub(User, 'findOne').resolves(userIfos as User);
+      });
+
+      after(() => {
+        sinon.restore();
+      });
+
+      it('Deve efetuar o login com sucesso', async () => {
+        const response = await chai.request(app).post('/login').send(userLogin);
+        chai.expect(response.status).to.equal(200);
+        chai.expect(response.body).to.have.property('token')
+      });
+
+      it('Deve retornar uma bad request se o password for informado errado', async () => {
+        const response = await chai.request(app).post('/login').send(incorrectLogins[3]);
+        chai.expect(response.status).to.equal(401);
+        chai.expect(response.body).to.deep.equal({ message: incorrectDataMessage });
+      });
     });
 
-    after(() => {
-      sinon.restore();
+    describe('Quando a função findOne falha ao fazer uma busca', () => {
+      before(() => {
+        sinon.stub(User, 'findOne').resolves(null);
+      });
+
+      after(() => {
+        sinon.restore();
+      });
+
+      it('Deve retornar uma bad request se o email não for informado', async () => {
+        const response = await chai.request(app).post('/login').send(incorrectLogins[0]);
+        chai.expect(response.status).to.equal(400);
+        chai.expect(response.body).to.deep.equal({ message: fieldFillMessage });
+      });
+
+      it('Deve retornar uma bad request se o password não for informado', async () => {
+        const response = await chai.request(app).post('/login').send(incorrectLogins[1]);
+        chai.expect(response.status).to.equal(400);
+        chai.expect(response.body).to.deep.equal({ message: fieldFillMessage });
+      });
+
+      it('Deve retornar uma bad request se o email for informado errado', async () => {
+        const response = await chai.request(app).post('/login').send(incorrectLogins[2]);
+        chai.expect(response.status).to.equal(401);
+        chai.expect(response.body).to.deep.equal({ message: incorrectDataMessage });
+      });
     });
 
-    it('Deve efetuar o login com sucesso', async () => {
-      const response = await chai.request(app).post('/login').send(userLogin);
-      chai.expect(response.status).to.equal(200);
-      chai.expect(response.body).to.have.property('token')
+    describe('Quando ocorre alguma falha no servidor', () => {
+      before(() => {
+        sinon.stub(User, 'findOne').throws(new Error('Algo deu errado!!'))
+      });
+
+      after(() => {
+        sinon.restore();
+      });
+
+      it('Deve retornar um erro', async () => {
+        const response = await chai.request(app).post('/login').send(userLogin);
+        chai.expect(response.status).to.equal(500);
+        chai.expect(response.body).to.deep.equal({ message: 'Algo deu errado!!' });
+      })
     });
   });
 });
