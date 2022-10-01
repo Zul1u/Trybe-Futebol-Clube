2decf58fe1715a49433630f844c3ae28a0ac7db2
Arthur <martinsarthur07@gmail.com>
2022-09-30 23:21:12 -0300

    feat: cria os testes da rota GET /login/validate

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -4,40 +4,28 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
 import User from '../database/models/User';
 import { after, before } from 'mocha';
+import {
+  fieldFillMessage,
+  incorrectDataMessage,
+  incorrectLogins,
+  invalidToken,
+  mockToken,
+  payload,
+  SERVER_ERROR,
+  tokenNotFound,
+  userIfos,
+  userLogin
+} from './mocks/login.mocks';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const userIfos = {
-  id: 2,
-  username: 'User',
-  role: 'user',
-  email: 'user@user.com',
-  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
-}
-
-const userLogin = {
-  email: 'user@user.com',
-  password: 'secret_user'
-}
-
-const incorrectLogins = [
-  { password: 'secret_user' },
-  { email: 'user@user.com', },
-  { email: 'iuser@iuser.com', password: 'secret_user' },
-  { email: 'user@user.com', password: 'secret_use' }
-];
-
-const fieldFillMessage = 'All fields must be filled';
-const incorrectDataMessage = 'Incorrect email or password';
-
-describe('/login', () => {
+describe('Rota /login', () => {
   describe('POST', () => {
     describe('Quando a função findOne faz uma busca com sucesso', () => {
       before(() => {
@@ -45,7 +33,7 @@ describe('/login', () => {
       });
 
       after(() => {
-        sinon.restore();
+        (User.findOne as sinon.SinonStub ).restore();
       });
 
       it('Deve efetuar o login com sucesso', async () => {
@@ -67,7 +55,7 @@ describe('/login', () => {
       });
 
       after(() => {
-        sinon.restore();
+        (User.findOne as sinon.SinonStub ).restore();
       });
 
       it('Deve retornar uma bad request se o email não for informado', async () => {
@@ -91,18 +79,70 @@ describe('/login', () => {
 
     describe('Quando ocorre alguma falha no servidor', () => {
       before(() => {
-        sinon.stub(User, 'findOne').throws(new Error('Algo deu errado!!'))
+        sinon.stub(User, 'findOne').throws(new Error(SERVER_ERROR))
       });
 
       after(() => {
-        sinon.restore();
+        (User.findOne as sinon.SinonStub ).restore();
       });
 
       it('Deve retornar um erro', async () => {
         const response = await chai.request(app).post('/login').send(userLogin);
         chai.expect(response.status).to.equal(500);
-        chai.expect(response.body).to.deep.equal({ message: 'Algo deu errado!!' });
+        chai.expect(response.body).to.deep.equal({ message: SERVER_ERROR });
       })
+    });
+  });
+});
+
+describe('Rota /login/validate', () => {
+  describe('GET', () => {
+    describe('Quando é passado um token valido', () => {
+      before(() => {
+        sinon.stub(User, 'findOne').resolves(userIfos as User);
+      });
+  
+      after(() => {
+        (User.findOne as sinon.SinonStub ).restore();
+      });
+  
+      it('Deve retornar a role do usuario', async () => {
+        const response = await chai.request(app).get('/login/validate').set('Authorization', mockToken);
+        chai.expect(response.status).to.equal(200);
+        chai.expect(response.body).to.deep.equal({ role: payload.role });
+      });
+    });
+
+    describe('Quando é passado um token invalido', () => {
+      before(() => {
+        sinon.stub(User, 'findOne').resolves(userIfos as User);
+      });
+  
+      after(() => {
+        (User.findOne as sinon.SinonStub ).restore();
+      });
+  
+      it('Deve retornar a mensagem "Invalid token"', async () => {
+        const response = await chai.request(app).get('/login/validate').set('Authorization', 'token123');
+        chai.expect(response.status).to.equal(401);
+        chai.expect(response.body).to.deep.equal({ message: invalidToken });
+      });
+    });
+
+    describe('Quando não é passado um token', () => {
+      before(() => {
+        sinon.stub(User, 'findOne').resolves(userIfos as User);
+      });
+  
+      after(() => {
+        (User.findOne as sinon.SinonStub ).restore();
+      });
+  
+      it('Deve retornar a mensagem "Token not found"', async () => {
+        const response = await chai.request(app).get('/login/validate')
+        chai.expect(response.status).to.equal(401);
+        chai.expect(response.body).to.deep.equal({ message: tokenNotFound });
+      });
     });
   });
 });
---
+++ app/backend/src/tests/mocks/login.mocks.ts
@@ -0,0 +1,43 @@
+import * as bcrypt from 'bcryptjs';
+import Token from '../../helpers/token.helper';
+
+const tokenGenerator = new Token();
+
+const EMAIL = 'kleitin@kleiton.com';
+const PASSWORD = 'superSecretPassword';
+
+export const SERVER_ERROR = 'Algo deu errado!!'
+
+export const userIfos = {
+  id: 2,
+  username: 'KleitinhoGamer22',
+  role: 'user',
+  email: EMAIL,
+  password: bcrypt.hashSync(PASSWORD)
+}
+
+export const userLogin = {
+  email: EMAIL,
+  password: PASSWORD
+}
+
+export const incorrectLogins = [
+  { password: PASSWORD },
+  { email: EMAIL, },
+  { email: 'email@email.com', password: PASSWORD },
+  { email: EMAIL, password: 'password' }
+];
+
+export const fieldFillMessage = 'All fields must be filled';
+export const incorrectDataMessage = 'Incorrect email or password';
+
+export const payload = {
+  id: 2,
+  username: 'KleitinhoGamer22',
+  role: 'user',
+}
+
+export const mockToken = tokenGenerator.createToken(payload);
+
+export const invalidToken = 'Invalid token';
+export const tokenNotFound = 'Token not found';
