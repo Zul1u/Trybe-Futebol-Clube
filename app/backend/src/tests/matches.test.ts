1fe0399bb0262943e96b0ba2e477293d405f5548
Arthur <martinsarthur07@gmail.com>
2022-10-01 18:11:23 -0300

    feat: adiciona testes para a rota /matches/matches?inProgress=?

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -7,8 +7,11 @@ import { app } from '../app';
 
 import { Response } from 'superagent';
 import Match from '../database/models/Match';
-import { matchesList } from './mocks/matches.mocks';
-import Team from '../database/models/Team';
+import {
+  matchesList,
+  matchesListInProgress,
+  matchesListNotProgress,
+} from './mocks/matches.mocks';
 
 chai.use(chaiHttp);
 
@@ -29,5 +32,55 @@ describe('GET', () => {
       expect(response.status).to.equal(200);
       expect(response.body).to.deep.equal(matchesList);
     })
+  });
+
+  describe('Rota /matches/matches?inProgress=?', () => {
+    describe('Quando é passado true para a query', () => {
+      before(() => {
+        sinon.stub(Match, 'findAll').resolves(matchesListInProgress as Match[]);
+      });
+  
+      after(() => {
+        (Match.findAll as sinon.SinonStub).restore();
+      });
+  
+      it('Deve retornar uma lista com todas as partidas que estão em progreço', async () => {
+        const response = await chai.request(app).get('/matches/matches?inProgress=true');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.deep.equal(matchesListInProgress);
+      });
+    });
+
+    describe('Quando é passado false para a query', () => {
+      before(() => {
+        sinon.stub(Match, 'findAll').resolves(matchesListNotProgress as Match[]);
+      });
+  
+      after(() => {
+        (Match.findAll as sinon.SinonStub).restore();
+      });
+  
+      it('Deve retornar uma lista com todas as partidas que não estão em progreço', async () => {
+        const response = await chai.request(app).get('/matches/matches?inProgress=false');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.deep.equal(matchesListNotProgress);
+      });
+    });
+
+    describe('Quando não é passado uma query', () => {
+      before(() => {
+        sinon.stub(Match, 'findAll').resolves(matchesList as Match[]);
+      });
+  
+      after(() => {
+        (Match.findAll as sinon.SinonStub).restore();
+      });
+  
+      it('Deve retornar uma lista com todas as partidas', async () => {
+        const response = await chai.request(app).get('/matches/matches?inProgress=');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.deep.equal(matchesList);
+      });
+    });
   });
 });
--- app/backend/src/tests/mocks/matches.mocks.ts
+++ app/backend/src/tests/mocks/matches.mocks.ts
@@ -1,4 +1,4 @@
-const match = {
+const matchNotInProgress = {
   id: 1,
   homeTeam: 16,
   homeTeamGoals: 1,
@@ -13,4 +13,22 @@ const match = {
   }
 };
 
-export const matchesList = [match, match, match, match];
+const matchInprogress = {
+  id: 2,
+  homeTeam: 16,
+  homeTeamGoals: 1,
+  awayTeam: 8,
+  awayTeamGoals: 1,
+  inProgress: true,
+  teamHome: {
+    teamName: 'São Paulo'
+  },
+  teamAway: {
+    teamName: 'Grêmio'
+  }
+};
+
+export const matchesList = [matchInprogress, matchInprogress, matchNotInProgress, matchNotInProgress];
+
+export const matchesListInProgress = [matchInprogress, matchInprogress];
+export const matchesListNotProgress = [matchNotInProgress, matchNotInProgress];
